f1fdf826864b0266b4629cb8c36c684503a5d27c
Quin Nordmark <quinnordmark@gmail.com>
2023-10-18 04:00:49 -0700

    beatbox part 1 compltete

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -5,6 +5,16 @@ require './lib/linked_list'
 class BeatBox
     attr_reader :list
     def initialize()
-        list = LinkedList.new()
+        @list = LinkedList.new()
+    end
+
+    def count
+        @list.count
+    end
+
+    def append(long_str)
+        long_str.split.each do |short_str|
+            list.append(short_str)
+        end
     end
 end
--- spec/iteration3.rb
+++ spec/iteration3.rb
@@ -7,5 +7,6 @@ RSpec.describe BeatBox do
         bb = BeatBox.new
         bb.append("deep doo ditt")
         expect(bb.list.to_string).to eq("deep doo ditt")
+        expect(bb.count).to eq(3)
     end
 end
