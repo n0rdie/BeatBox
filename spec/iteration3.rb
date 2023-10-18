ff47dd7d59895a94df0dfc90d0e862a8fc35492c
Quin Nordmark <quinnordmark@gmail.com>
2023-10-18 03:56:36 -0700

    BeatBox append test

---
+++ spec/iteration3.rb
@@ -0,0 +1,11 @@
+require "./lib/linked_list"
+require "./lib/node"
+require "./lib/beat_box"
+
+RSpec.describe BeatBox do
+    it "append" do
+        bb = BeatBox.new
+        bb.append("deep doo ditt")
+        expect(bb.list.to_string).to eq("deep doo ditt")
+    end
+end
