be455d5159a0d3a72b2d288d6b8bd1351a52cf9f
Quin Nordmark <quinnordmark@gmail.com>
2023-10-18 03:20:26 -0700

    LinkedList find test

--- spec/iteration2.rb
+++ spec/iteration2.rb
@@ -2,7 +2,7 @@ require "./lib/linked_list"
 require "./lib/node"
 
 RSpec.describe LinkedList do
-    it "prepend" do
+    xit "prepend" do
         list = LinkedList.new
         list.append("plop")
         list.append("suu")
@@ -11,12 +11,22 @@ RSpec.describe LinkedList do
         expect(list.count).to eq(3)
     end
 
-    it "insert" do
+    xit "insert" do
         list = LinkedList.new
         list.append("plop")
         list.append("suu")
         list.prepend("dop")
         list.insert(1, "woo")
         expect(list.to_string).to eq("dop woo plop suu")
+    end
+
+    it "find" do
+        list = LinkedList.new
+        list.append("plop")
+        list.append("suu")
+        list.prepend("dop")
+        list.insert(1, "woo")
+        # "dop woo plop suu"
+        expect(list.find(1,2)).to eq("suu dop")
     end
 end
