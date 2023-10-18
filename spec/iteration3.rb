a1a0f8a05d24436b9f6ac4af240e965fc9613f57
Quin Nordmark <quinnordmark@gmail.com>
2023-10-18 04:03:04 -0700

    BeatBox play test

--- spec/iteration3.rb
+++ spec/iteration3.rb
@@ -9,4 +9,10 @@ RSpec.describe BeatBox do
         expect(bb.list.to_string).to eq("deep doo ditt")
         expect(bb.count).to eq(3)
     end
+    it "plays" do
+        bb = BeatBox.new
+        bb.append("deep doo ditt")
+        bb.append("woo hoo shu")
+        expect(bb.plays).to eq("plays the sounds deep doo ditt woo hoo shu")
+    end
 end
