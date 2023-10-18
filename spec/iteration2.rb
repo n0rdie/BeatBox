9334787e06877c484bfc130a7ca6e83b1ad28622
Quin Nordmark <quinnordmark@gmail.com>
2023-10-18 03:33:10 -0700

    git add spec/! exit clear

--- spec/iteration2.rb
+++ spec/iteration2.rb
@@ -20,7 +20,7 @@ RSpec.describe LinkedList do
         expect(list.to_string).to eq("dop woo plop suu")
     end
 
-    it "find" do
+    xit "find" do
         list = LinkedList.new
         list.append("plop")
         list.append("suu")
@@ -28,5 +28,16 @@ RSpec.describe LinkedList do
         list.insert(1, "woo")
         # "dop woo plop suu"
         expect(list.find(1,2)).to eq("woo plop")
+    end
+
+    it "includes" do
+        list = LinkedList.new
+        list.append("plop")
+        list.append("suu")
+        list.prepend("dop")
+        list.insert(1, "woo")
+        # "dop woo plop suu"
+        expect(list.includes("dop")).to eq(true)
+        expect(list.includes("nope")).to eq(false)
     end
 end
